fcae636221fb654a336055bead09bc4abe25ecc0
JesperSunden <jespersunden@telia.com>
2018-10-29 00:05:39 +0100

    Added modular admin pages and subpages

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,18 +6,52 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+    public $settings;
+
+    public $pages = array();
+    public $subpages = array();
+
+    public function __construct() {
+
+        $this->settings = new SettingsApi();
+
+        $this->pages = array(
+            array(
+                'page_title'    => 'Job Listing',
+                'menu_title'    => 'Job Listing',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'rcit_job_listings',
+                'callback'      => function() { echo '<h1>Settings</h1>'; },
+                'icon_url'      => 'dashicons-nametag',
+                'position'      => 110
+            ),
+        );
+
+        $this->subpages = array(
+            array(
+                'parent_slug'   => 'rcit_job_listings',
+                'page_title'    => 'Users',
+                'menu_title'    => 'Users',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'rcit_job_listings_users',
+                'callback'      => function() { echo '<h1>Users</h1>'; }
+            ),
+            array(
+                'parent_slug'   => 'rcit_job_listings',
+                'page_title'    => 'Jobs',
+                'menu_title'    => 'Jobs',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'rcit_job_listings_jobs',
+                'callback'      => function() { echo '<h1>Jobs</h1>'; }
+            ),
+        );
+    }
+
     public function register() {
-        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-    }
-
-    public function add_admin_pages() {
-            add_menu_page( 'Job Listing', 'Job Listing', 'manage_options', 'rcit_job_listings', array( $this, 'admin_index' ), 'dashicons-nametag', 110 );
-    }
-
-    public function admin_index() {
-        require_once $this->plugin_path . 'templates/admin-page.php';
+        $this->settings->add_pages( $this->pages )->with_sub_page( 'Settings' )->add_subpages( $this->subpages )->register();
     }
 }
